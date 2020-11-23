aea5dc7158e19f59a2f7f3135c723b02e5d3e165
Noa <noa.aizer@gmail.com>
2020-11-23 14:25:44 +0200

    Initial commit

---
+++ app/src/main/java/com/example/thelibrary/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.thelibrary;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.thelibrary.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.b);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSecondActivity();
+            }
+        });
+    }
+
+    private void goToSecondActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+
+    }
+
+}
