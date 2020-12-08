37425defbb3af0b8b13b9e1fa875c431fa54672d
Noa <noa.aizer@gmail.com>
2020-12-08 12:24:50 +0200

    add adminMenu and some edits in other activities

---
+++ app/src/main/java/com/example/thelibrary/activities/MenuAdminActivity.java
@@ -0,0 +1,51 @@
+package com.example.thelibrary.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.thelibrary.R;
+
+public class MenuAdminActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button loansList_btn,booksList_btn,deliveres_btn, clientsList_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_admin);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        loansList_btn = (Button)findViewById(R.id.loansList);
+        booksList_btn = (Button)findViewById(R.id.booksList);
+        deliveres_btn = (Button)findViewById(R.id.deliveres);
+        clientsList_btn = (Button)findViewById(R.id.clientsList);
+
+
+        loansList_btn.setOnClickListener(this);
+        booksList_btn.setOnClickListener(this);
+        deliveres_btn.setOnClickListener(this);
+        clientsList_btn.setOnClickListener(this);
+
+    }
+    public void onClick(View v) {
+        if(v==loansList_btn){
+
+        }
+        if(v==booksList_btn){
+            Intent intent = new Intent(MenuAdminActivity.this, BooksListAdminActivity.class);
+            startActivity(intent);
+
+        }
+        if(v==deliveres_btn){
+
+        }
+        if(v==clientsList_btn){
+
+        }
+    }
+
+}
--- app/src/main/java/com/example/thelibrary/activities/MenuUserActivity.java
+++ app/src/main/java/com/example/thelibrary/activities/MenuUserActivity.java
@@ -3,16 +3,16 @@ package com.example.thelibrary.activities;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.thelibrary.R;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
 public class MenuUserActivity extends AppCompatActivity implements View.OnClickListener {
     private Button loan_btn,myDetails_btn,myBooks_btn, searchBook_btn;
-    private ImageButton editBooks_btn;
+    private ExtendedFloatingActionButton editBooks_btn ,labInf_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +23,15 @@ public class MenuUserActivity extends AppCompatActivity implements View.OnClickL
         myDetails_btn = (Button)findViewById(R.id.userDetails);
         myBooks_btn = (Button)findViewById(R.id.myBooks);
         searchBook_btn = (Button)findViewById(R.id.searchAbook);
-        editBooks_btn =(ImageButton) findViewById(R.id.editBooks);
+        editBooks_btn = (ExtendedFloatingActionButton) findViewById(R.id.editBooks);
+        labInf_btn = (ExtendedFloatingActionButton) findViewById(R.id.information);
 
         loan_btn.setOnClickListener(this);
         myDetails_btn.setOnClickListener(this);
         myBooks_btn.setOnClickListener(this);
         searchBook_btn.setOnClickListener(this);
         editBooks_btn.setOnClickListener(this);
+        labInf_btn.setOnClickListener(this); //library information
     }
     public void onClick(View v) {
         if(v==loan_btn){
@@ -42,6 +44,9 @@ public class MenuUserActivity extends AppCompatActivity implements View.OnClickL
 
         }
         if(v==searchBook_btn){
+
+        }
+        if(v==labInf_btn){
 
         }
     }
